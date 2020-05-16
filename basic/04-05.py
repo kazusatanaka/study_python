cccb1cc5d091167678e6168991ef03546a5355ee
kazusatanaka <tanakakazusa@softbank.ne.jp>
2020-05-16 15:06:37 +0900

    04-05^03

--- basic/04-05.py
+++ basic/04-05.py
@@ -12,3 +12,16 @@ for a in range(1,10+1):
         print("×")
     if (a%2==0) and ( a%3==0):
         print("△")
+
+
+for a in range(1,10+1):
+    if (a%12==0):
+        print("〇")
+    else:
+        if(a%4==0):
+            print("△")
+        else:
+            if(a%2==0):
+                print("×")
+            else:
+                print("☆")

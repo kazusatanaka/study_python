8b8498bbff4784ef0755c039fcc1e8c89da93e2e
kazusatanaka <tanakakazusa@softbank.ne.jp>
2020-05-16 14:57:55 +0900

    04-05-02

--- basic/04-05.py
+++ basic/04-05.py
@@ -3,3 +3,12 @@ for a in range(1,10+1):
         print("小さいです")
     else:
         print("大きいです")
+
+for a in range(1,10+1):
+    print(a)
+    if a%2==0:
+        print("〇")
+    if a%3==0:
+        print("×")
+    if (a%2==0) and ( a%3==0):
+        print("△")
